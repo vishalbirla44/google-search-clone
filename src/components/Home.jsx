1fd53f2561f415331b036799810f910f0055726e
vishal birla <vishalbirla26487@gmail.com>
2024-05-24 14:15:18 +0000

    vv

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HomeHeader from './HomeHeader'
 import Logo from '../assets/google-logo.png'
 import SearchInput from './SearchInput'
+import Footer from './Footer'
 
 const Home = () => {
   return (
@@ -11,8 +12,14 @@ const Home = () => {
         <div className='w-full px-5 flex flex-col items-center mt-44'>
             <img src={Logo} className='w-[172px] md:w-[272px] mb-8'/>
             <SearchInput/>
+            <div className='flex gap-1 text-[#3c4043] mt-8'>
+              <button className='h-9 px-4 bg-[#f8f9fa] text-sm rounded-md border border-[#f8f9fa] hover:border-[#dadce0] hover:shadow-c2 '> Google Search</button>
+
+              <button className='h-9 px-4 bg-[#f8f9fa] text-sm rounded-md border border-[#f8f9fa] hover:border-[#dadce0] hover:shadow-c2 '> I'm Feeling Lucky</button>
+            </div>
         </div>
         </main>
+        <Footer/>
     </div>
   )
 }
